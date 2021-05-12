d7fcdb7ecc967db3baaf870a8c4cfa099bc0139c
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-11 19:43:11 -0500

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -2,12 +2,7 @@ var express = require('express');
 
 var app = express();
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-var session = require('express-session');
 var bodyParser = require('body-parser');
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({ secret: 'cryptoisascam' }));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
