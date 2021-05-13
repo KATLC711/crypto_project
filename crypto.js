2b71086817acd1037876ef12af88a53351d74700
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-12 22:27:42 -0500

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -75,8 +75,7 @@ app.get('/home', function (req, res) {
 
         for (var i = 0; i < holdings.length; i++) {
 
-            var price = request('https://api.cryptonator.com/api/full/' + holdings[i] + '-usd', handleGet);
-            console.log(price.ticker)
+            request('https://api.cryptonator.com/api/full/' + holdings[i] + '-usd', handleGet);
             cryprolist.push({ 'holdings': holdings[i], 'amount': amount[i], 'price': price })
         }
         var context = []
@@ -86,7 +85,7 @@ app.get('/home', function (req, res) {
 
         function handleGet(err, response, body) {
             if (!err && response.statusCode < 400) {
-                return body
+                console.log(body.price)
             } else {
                 console.log(err);
                 console.log(response.statusCode);
