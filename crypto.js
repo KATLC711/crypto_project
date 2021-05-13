91caf8b337b8701d1707215dd9c5ce0ec8e3dcd8
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-12 22:57:18 -0500

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -80,7 +80,7 @@ app.get('/home', function (req, res) {
             function handleGet(err, response, body) {
                 if (!err && response.statusCode < 400) {
 
-                    var price = JSON.parse(body.ticker);
+                    var price = JSON.parse(body[0]);
                     console.log(price)
                 } else {
                     console.log(err);
