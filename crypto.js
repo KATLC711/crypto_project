d39a2cbc910a366bc46878fc5cf644ff25e6f76b
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-13 21:12:43 -0500

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -78,7 +78,7 @@ app.get('/home', function (req, res) {
         var amount = user_info[1].amount
         var cryprolist = []
 
-        for (let i = 0, p = Promise.resolve(); i < holding; i++) {
+        for (let i = 0, p = Promise.resolve(); i < holdings.length; i++) {
             p = p.then(_ => new Promise(resolve =>
                 function () {
                     console.log(i);
