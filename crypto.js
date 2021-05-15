699db085449b62646a8ee6069d9bc59b521986d9
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-15 17:05:50 -0500

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -192,7 +192,7 @@ app.post('/purchase-order', function (req, res) {
             for (i = 0; i < cred_info.length; i++) {
                 if (user_info[1].username == cred_info[i].username) {
                     user_info[1].holdings.push(crypto_name_purchased)
-                    user_info[1].amount.push(parsefloat(crypto_amount_purchased))
+                    user_info[1].amount.push(parseFloat(crypto_amount_purchased))
                 }
             }
         }
