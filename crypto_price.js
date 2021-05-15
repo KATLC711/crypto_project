cbb4ee701b3f02647025f5d8576350a12ad4c1f1
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-14 20:19:13 -0500

    Update crypto_price.js

--- crypto_price.js
+++ crypto_price.js
@@ -15,11 +15,7 @@ var promises = []
 
 for (i = 0; i < crypto_list.length; i++) {
     promises.push(
-        axios.get('https://api.cryptonator.com/api/full/' + crypto_list[i] + '-usd').then(response => {
-            // do something with response
-            crypto_price.push(response.data.ticker.price)
-            //crypto_price.push(response.data.ticker.price);
-        })
+        console.log(crypto_list[i])
     )
 }
 
